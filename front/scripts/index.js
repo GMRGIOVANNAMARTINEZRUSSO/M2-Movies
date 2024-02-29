35abaf7ec8b0556f16b237ad7ada798c15975876
giovanna <giovi92@hotmail.com>
2024-02-29 19:51:37 -0300

    my second proyect

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,11 @@
 console.log(tempData);
+
+// contenedor tarjetas de películas
+const container = document.getElementById('container');
+//$.get("url", (data)=>{
+    //renderCards(data)
+//})
+
+//const renderCards= ()=>{
+
+// Mapea tarjeta para cada película
--- front/scripts/tempData.js
+++ front/scripts/tempData.js
@@ -29,4 +29,63 @@ const tempData = [
     poster:
       "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
   },
+  {
+    title: "Harry Potter and the Chamber of Secrets",
+    year: 2002,
+    director: "Chris Columbus",
+    duration: "2h 41min",
+    genre: ["Adventure", "Family", "Fantasy", "Mystery"],
+    rate: 8.7,
+    poster:
+      "https://m.media-amazon.com/images/I/51UizOJ+NFL.jpg",
+  },
+  {
+    title: "Forrest Gump",
+    year: 1994,
+    director: "Robert Zemeckis",
+    duration: "2h 22min",
+    genre: ["	Comedia", "Drama",],
+    rate: 8.8,
+    poster:
+      "https://m.media-amazon.com/images/I/71dViWh3lnL._SL1500_.jpg",
+  },
+  {
+    title: "Iron Man",
+    year: 2008,
+    director: "Jon Favreau",
+    duration: "2h 6min",
+    genre: ["Acción", "ciencia ficción", "aventuras", "cine de superhéroes"],
+    rate: 7.9,
+    poster:
+      "https://m.media-amazon.com/images/I/71PYIDCnySL._SL1372_.jpg",
+  },
 ];
+// Mapea tarjeta para cada película
+const movieCards = tempData.map(movie => {
+  const card = document.createElement('div');
+  card.classList.add('movie-card'); 
+
+const img = document.createElement('img');
+ img.src = movie.poster;
+ card.appendChild(img);
+  
+const content = document.createElement('div');
+  content.classList.add('movie-card-content');
+  content.innerHTML= `
+      <a href="${movie.title}"><h2>${movie.title}</h2></a>
+      <p><strong>Año:</strong> ${movie.year}</p>
+      <p><strong>Director:</strong> ${movie.director}</p>
+      <p><strong>duration:</strong>${movie.duration}</p>
+      <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+      <p><strong>Rate:</strong> ${movie.rate}</p>
+        `;
+  card.appendChild(content);
+  return card;
+});
+
+// Agrega las tarjetas al contenedor
+movieCards.forEach(card => {
+  container.appendChild(card);
+});
+
+

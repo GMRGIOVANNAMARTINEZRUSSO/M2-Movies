bf7b16133d1477dc1a8ecefa26810764e7496893
giovanna <giovi92@hotmail.com>
2024-03-09 21:32:35 -0300

    hw bootstrap

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,37 +1,46 @@
-const container = document.getElementById('container');
 
-$.get("https://webpt19b.web.app/data/movies.json", (data)=>{
-    renderCards(data);
+//const renderCards = require("./renderCards")
+// main.js
+//const { fetchData, renderCards } = require('./module.js');
+
+
+fetchData("https://students-api.2.us-1.fl0.io/movies", (data, status) => {
+  renderCards(data);
 });
 
-  const renderCards= (data) => {
-        const movieCards = data.map(movie => {
-            const card = document.createElement('div');
-            card.classList.add('movie-card'); 
-          
-          const img = document.createElement('img');
-           img.src = movie.poster;
-           card.appendChild(img);
-            
-          const content = document.createElement('div');
-            content.classList.add('movie-card-content');
-            content.innerHTML= `
-                <a href="${movie.title}"><h2>${movie.title}</h2></a>
-                <p><strong>Año:</strong> ${movie.year}</p>
-                <p><strong>Director:</strong> ${movie.director}</p>
-                <p><strong>duration:</strong>${movie.duration}</p>
-                <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
-                <p><strong>Rate:</strong> ${movie.rate}</p>
-                  `;
-            card.appendChild(content);
-            return card;
-          });
-          
-          // Agrega las tarjetas al contenedor
-          movieCards.forEach(card => {
-            container.appendChild(card);
-          });          
-    }
+
+// const container = document.getElementById('container');
+// $.get("https://students-api.2.us-1.fl0.io/movies", (data, status)=>{
+//     renderCards(data);
+// });
 
 
 
+// // const renderCards= (data) => {
+//     const movieCards = data.map(movie => {
+//         const card = document.createElement('div');
+//         card.classList.add('movie-card'); 
+      
+//       const img = document.createElement('img');
+//        img.src = movie.poster;
+//        card.appendChild(img);
+        
+//       const content = document.createElement('div');
+//         content.classList.add('movie-card-content');
+//         content.innerHTML= `
+//             <a href="${movie.title}"><h2>${movie.title}</h2></a>
+//             <p><strong>Año:</strong> ${movie.year}</p>
+//             <p><strong>Director:</strong> ${movie.director}</p>
+//             <p><strong>duration:</strong>${movie.duration}</p>
+//             <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+//             <p><strong>Rate:</strong> ${movie.rate}</p>
+//               `;
+//         card.appendChild(content);
+//         return card;
+//       });
+      
+//       // Agrega las tarjetas al contenedor
+//       movieCards.forEach(card => {
+//         container.appendChild(card);
+//       });          
+// }
---
+++ front/scripts/renderCards.js
@@ -0,0 +1,41 @@
+// module.js
+//const $ = require('jquery');
+const container = document.getElementById('container');
+
+const fetchData = (url, callback) => {
+  $.get(url, callback);
+};
+
+const renderCards = (data) => {
+  const movieCards = data.map(movie => {
+    const card = document.createElement('div');
+    card.classList.add('movie-card');
+
+    const img = document.createElement('img');
+    img.src = movie.poster;
+    card.appendChild(img);
+
+    const content = document.createElement('div');
+    content.classList.add('movie-card-content');
+    content.innerHTML= `
+      <a href="${movie.title}"><h2>${movie.title}</h2></a>
+      <p><strong>Año:</strong> ${movie.year}</p>
+      <p><strong>Director:</strong> ${movie.director}</p>
+      <p><strong>duration:</strong>${movie.duration}</p>
+      <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
+      <p><strong>Rate:</strong> ${movie.rate}</p>
+    `;
+    card.appendChild(content);
+    return card;
+  });
+
+  // Agrega las tarjetas al contenedor
+  movieCards.forEach(card => {
+    container.appendChild(card);
+  });
+};
+
+module.exports = {
+  fetchData,
+  renderCards
+};
